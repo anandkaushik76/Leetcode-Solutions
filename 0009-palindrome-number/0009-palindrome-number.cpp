4270c5d35bc53a2aba623b37143c2fdd61c1f429
Anand Kaushik <anandrkkaushik786@gmail.com>
2026-02-17 18:23:10 +0530

    Time: 1 ms (57.79%), Space: 8.5 MB (91.37%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0)){
+            return false;
+        }
+
+        int dup=x;
+        long rev=0;
+        while(x>0){
+            int ld=x%10;
+            rev=rev*10+ld;
+            x=x/10;
+        }
+        if(rev==dup)  return true;
+        else      return false;
+    }
+};
