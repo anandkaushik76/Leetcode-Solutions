68ecc95b1ed9cde31c99a3045d23f352b16eb408
Anand Kaushik <anandrkkaushik786@gmail.com>
2026-03-03 21:53:41 +0530

    Time: 0 ms (100%), Space: 12.6 MB (8.75%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>result(m+n);
+       std:: merge(nums1.begin(),nums1.begin()+m,nums2.begin(),nums2.begin()+n,result.begin());
+        nums1=result;
+    }
+};
